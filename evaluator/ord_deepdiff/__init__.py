9ba6256bc0d5d16e322c528820ea9f010d55b4ce
qai <qai@mit.edu>
2023-08-12 02:09:47 -0400

    add diff for list of reaction workups

---
+++ evaluator/ord_deepdiff/__init__.py
@@ -0,0 +1,6 @@
+from .utils import *
+from .base import DiffReportKind
+from .list_of_compounds import list_of_compounds_greedy_matcher, diff_list_of_compounds, DiffReportListOfCompounds
+from .list_of_compound_lists import diff_list_of_compound_lists, DiffReportCompoundLol
+from .list_of_reaction_workups import diff_list_of_reaction_workups, DiffReportListOfReactionWorkups
+from .reaction_conditions import diff_reaction_conditions, DiffReportReactionConditions
