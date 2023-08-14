ef5572d5382775afca81af7a399fe72ebc69e9f8
qai <qai@mit.edu>
2023-08-14 16:43:43 -0400

    formatted results

---
+++ models_llama/adapter/finetune_20230731/evaluate_infer.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from evaluator import *
+
+
+def run_eval(filename: FilePath, slice_indices: tuple[int, int] | None,
+             skip_rule=FieldSkipRule.ignore_absent_in_prompt_with_exceptions):
+    res = Evaluator.evaluate_llama_inference_json(filename, slice_indices=slice_indices,
+                                                  skip_rule=skip_rule)
+    resport_summary = res["report_summary"]
+    df = resport_summary.get_table_compound_fields(from_inputs=True)
+    df.to_csv("eval__compound_inputs.csv")
+    df = resport_summary.get_table_compound_fields(from_inputs=False)
+    df.to_csv("eval__compound_outcomes.csv")
+    df = resport_summary.get_table_messages()
+    df.to_csv("eval__messages.csv")
+    return
+
+
+if __name__ == '__main__':
+    logger.remove(0)
+    logger.add(__file__.replace(".py", ".log"))
+    run_eval(
+        filename="infer-14.json",
+        slice_indices=None,
+        skip_rule=FieldSkipRule.ignore_absent_in_prompt_with_exceptions
+    )
+    # somehow # 1406 took much longer than others
